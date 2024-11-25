4e738389eb9fea0b664a41726de01a2e6c798c0d
Cynthia <46873955+curephoenix@users.noreply.github.com>
2024-11-25 13:21:39 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,8 +19,9 @@ LOG_FILE = "posted_images.log"  # Log file to track posted images
 
 def get_next_image(directory="images/"):
     """Retrieve the next unposted image to post based on numerical order."""
-    images = glob.glob({directory} + "*")
+    images = glob.glob(directory + "*")
     image_open = images[random.randint(0,len(images))-1]
+    image_size = os.path.getsize(image_open)
     if not images:
         print("No images found in the directory.")
         return None
